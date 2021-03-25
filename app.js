cdd6e906df77c18b8a924aa0c3be78cee3fe0531
syauqy <syauqilenterano@gmail.com>
2021-03-26 00:00:55 +0700

    change html to ejs

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('view engine','ejs')
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home')
 })
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
